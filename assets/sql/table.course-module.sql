522fd0a1c3792a48144ac6d465486ecf64622905
J Andrew Scott <jascott@programmer.net>
2020-10-05 15:40:23 -0400

    Require course module position

--- assets/sql/table.course-module.sql
+++ assets/sql/table.course-module.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `course_module` (
 	`course_id` int(10) UNSIGNED NOT NULL,
 	`module_id` int(10) UNSIGNED NOT NULL,
-	`position` tinyint(3) UNSIGNED DEFAULT NULL,
+	`position` tinyint(3) UNSIGNED NOT NULL,
 	PRIMARY KEY (`course_id`,`module_id`),
 	KEY `module_id` (`module_id`),
 	UNIQUE KEY `position` (`course_id`,`module_id`,`position`)
