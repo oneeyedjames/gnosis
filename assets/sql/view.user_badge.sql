fb26cc1c73e89efb1e400b6b0539ec3b3ae47dbb
J Andrew Scott <jascott@programmer.net>
2020-10-05 15:47:03 -0400

    Match filenames to table names

--- assets/sql/view.user_badge.sql
+++ assets/sql/view.user_badge.sql
@@ -1,4 +1,12 @@
 CREATE VIEW `user_badge` AS
+SELECT `s`.`id`, `us`.`user_id`, 'series' AS `type`,
+`s`.`title`, `s`.`alias`, `s`.`image`, `s`.`summary`,
+`us`.`score`, `us`.`total`, `us`.`percent`,
+`us`.`created_date`, `us`.`modified_date`
+FROM `series` AS `s`
+INNER JOIN `user_series` AS `us`
+ON `s`.`id` = `us`.`series_id`
+UNION
 SELECT `c`.`id`, `uc`.`user_id`, 'course' AS `type`,
 `c`.`title`, `c`.`alias`, `c`.`image`, `c`.`summary`,
 `uc`.`score`, `uc`.`total`, `uc`.`percent`,
