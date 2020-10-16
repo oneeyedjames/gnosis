3e38cf2a3180486bceaa0a13d510546d72dae9b7
Andrew Scott <andrew.scott@US-M0317-5827.attlocal.net>
2020-10-16 11:07:01 -0400

    Complete badge view

--- src/assets/sql/rel.category.sql
+++ src/assets/sql/rel.category.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `category`
-ADD CONSTRAINT `category_parent` FOREIGN KEY (`parent_id`) REFERENCES `category`(`id`);
+ADD CONSTRAINT `category_parent` FOREIGN KEY (`parent_id`) REFERENCES `category` (`id`);
--- src/assets/sql/view.badge.sql
+++ src/assets/sql/view.badge.sql
@@ -1,16 +1,13 @@
 CREATE VIEW `badge` AS
-SELECT `s`.`id`, 'series' AS `type`,
-`s`.`title`, `s`.`alias`, `s`.`summary`, `b`.`value`
+SELECT `s`.`id`, `s`.`category_id`, `s`.`difficulty_id`, 'series' AS `type`,
+`s`.`title`, `s`.`alias`, `s`.`image`, `s`.`summary`, `b`.`value`
 FROM `series` AS `s`
-INNER JOIN `series_badge` AS `b`
-ON `b`.`series_id` = `s`.`id`
-UNION SELECT `c`.`id`, 'course' AS `type`,
-`c`.`title`, `c`.`alias`, `c`.`summary`, `b`.`value`
+INNER JOIN `series_badge` AS `b` ON `b`.`series_id` = `s`.`id`
+UNION SELECT `c`.`id`, `c`.`category_id`, `c`.`difficulty_id`, 'course' AS `type`,
+`c`.`title`, `c`.`alias`, `c`.`image`, `c`.`summary`, `b`.`value`
 FROM `course` AS `c`
-INNER JOIN `course_badge` AS `b`
-ON `b`.`course_id` = `c`.`id`
-UNION SELECT `m`.`id`, 'module' AS `type`,
-`m`.`title`, `m`.`alias`, `m`.`summary`, `b`.`value`
+INNER JOIN `course_badge` AS `b` ON `b`.`course_id` = `c`.`id`
+UNION SELECT `m`.`id`, `m`.`category_id`, `m`.`difficulty_id`, 'module' AS `type`,
+`m`.`title`, `m`.`alias`, `m`.`image`, `m`.`summary`, `b`.`value`
 FROM `module` AS `m`
-INNER JOIN `module_badge` AS `b`
-ON `b`.`module_id` = `m`.`id`;
+INNER JOIN `module_badge` AS `b` ON `b`.`module_id` = `m`.`id`;
